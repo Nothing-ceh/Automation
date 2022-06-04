564ef57287749b9ee69949e8bcce38bf8ddfe794
Shib Nath <shibnath.ceh@gmail.com>
2022-06-05 02:05:51 +0530

    Some changes i can do that

--- userdel.sh
+++ userdel.sh
@@ -4,3 +4,13 @@
 
 #fist we need to set variables 
 
+echo "What is the user name which you want to delete"
+
+read user
+
+sudo userdel $user
+
+echo "User delete successfully"
+echo 
+
+tail -n 3 /etc/passwd
