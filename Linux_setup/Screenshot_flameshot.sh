f7c2d39410d8a5d2e4f82fd5b854134f40e51dea
ITadmin <itadmin@bigohtech>
2022-06-19 01:21:07 +0530

    Flameshot & Error_soulation folder add

---
+++ Linux_setup/Screenshot_flameshot.sh
@@ -0,0 +1,9 @@
+# Install  flameshot tool for screenshot.
+# Ubuntu based distribution
+wget https://github.com/flameshot-org/flameshot/releases/download/v0.9.0/flameshot-0.9.0-1.ubuntu-20.04.amd64.deb
+# Executebale permission
+ chmod u+x flameshot-0.9.0-1.ubuntu-20.04.amd64.deb
+ # Install 
+ sudo dpkg -i flameshot-0.9.0-1.ubuntu-20.04.amd64.deb
+ # remove flameshot download file 
+ rm -f flameshot-0.9.0-1.ubuntu-20.04.amd64.deb
